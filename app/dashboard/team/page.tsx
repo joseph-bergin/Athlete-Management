75ef5d848cacfd2bed2252df2a6ff864bcc3a2cb
Matt Couts <mattcouts7@gmail.com>
2024-09-09 14:59:49 -0500

    route to dashboard on login

--- app/dashboard/team/page.tsx
+++ app/dashboard/team/page.tsx
@@ -15,6 +15,22 @@ export default function Team() {
                     <p>Fields: Athlete Name, Position, Year</p>
                 </div>
             </div>
+            <div>
+                <p>3. Update Athlete Form (Opens in a sheet).</p>
+                <div className="px-8">
+                    <p>Fields: Athlete Name, Position, Year</p>
+                    <p>Confirmation: Are you sure you want to update this athlete?</p>
+                </div>
+            </div>
+            <div>
+                <p>4. Delete Athlete (Opens a modal).</p>
+                <div className="px-8">
+                    <p>Confirmation: Are you sure you want to delete this athlete?</p>
+                </div>
+            </div>
+            <div>
+                <p>Import Athletes from .xlsx or .csv Files (SheetJS)</p>
+            </div>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { NavHeader } from "@/components/nav-header"
 import { HeroSection } from "./hero"
 
 export default function Home() {
