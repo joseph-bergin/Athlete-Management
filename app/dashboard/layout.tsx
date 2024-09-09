fa1ffd8531ccf4a54374c8cf4c7bf87e8ece0a8e
Matt Couts <mattcouts7@gmail.com>
2024-09-09 14:19:17 -0500

    protect /dashboard/*

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,12 @@
+'use client'
+
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <div>
+    <div className="px-32">
         {children}
     </div>
   )
--- app/dashboard/team/page.tsx
+++ app/dashboard/team/page.tsx
@@ -1,7 +1,20 @@
 export default function Team() {
     return (
         <div>
-            <p>Team Page</p>
+            <h1 className="text-3xl font-extrabold tracking-tight lg:text-4xl">Team Manager</h1>
+            <div>
+                <p>1. Table containing list of athletes on team.</p>
+                <div className="px-8">
+                    <p>Columns: Athlete Name, Position, Year</p>
+                    <p>Features: Filter, Search, Paginator</p>
+                </div>
+            </div>
+            <div>
+                <p>2. Create Athlete Form (Opens in a sheet).</p>
+                <div className="px-8">
+                    <p>Fields: Athlete Name, Position, Year</p>
+                </div>
+            </div>
         </div>
     )
 }
