fb8013e3bf347842d8f42c2573fb4c2deb2a28f1
Matt Couts <mattcouts7@gmail.com>
2024-09-23 16:44:29 -0500

    add looker embed

--- app/dashboard/performance/page.tsx
+++ app/dashboard/performance/page.tsx
@@ -2,6 +2,7 @@ export default function Performance() {
     return (
         <div>
             <p>Performance Page</p>
+            <iframe width="600" height="450" src="https://lookerstudio.google.com/embed/reporting/9dbdbb4c-e522-4598-a40c-b81fb3d21983/page/M1rCE" allowFullScreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
         </div>
     )
 }
