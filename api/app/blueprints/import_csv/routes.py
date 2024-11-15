2a7b3d8db51f7ea2c0cd629f04ca5fb65e0bf08d
Dylan <dab023@uark.edu>
2024-11-15 13:16:52 -0600

    Force Frame csv upload

--- api/app/blueprints/import_csv/routes.py
+++ api/app/blueprints/import_csv/routes.py
@@ -22,7 +22,7 @@ def get_or_create_athlete(athlete_data):
         return response.data[0]['athleteID']
     else:
         # Athlete doesn't exist, insert into athlete table
-        new_athlete = {"first_name": first_name, "last_name": last_name, "teamID": athlete_data["teamID"]}
+        new_athlete = {"first_name": first_name, "last_name": last_name, "teamID": athlete_data["teamID"], "position_abbreviation": athlete_data["Position"]}
         insert_response = supabase.table('Athlete').insert(new_athlete).execute()
 
         return insert_response.data[0]['athleteID']
@@ -54,6 +54,52 @@ def upload_csv():
 
             # Step 3: Insert the record into your main table
             response = supabase.table('catapultdata').insert(record).execute()
+
+        return jsonify({"message": "Data inserted successfully!"}), 201
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+@import_csv_blueprint.route('/upload_force_frame_csv', methods=['POST'])
+def upload_force_frame_csv():
+    try:
+        data = request.json
+        if not data:
+            return jsonify({"error": "No data received"}), 400
+
+        for entry in data:
+            # Step 1: Get or create the athlete in the athlete table
+            athlete_id = get_or_create_athlete(entry)
+            # Step 2: Prepare the record to insert into the main table
+
+            record = {
+                "athleteID": athlete_id,
+                "dataDate": entry["Date UTC"],
+                "dataTime": entry["Time UTC"],
+                "deviceName": entry["Device"],
+                "mode": entry["Mode"],
+                "test": entry["Test"],
+                "direction": entry["Direction"],
+                "angle": entry["Angle"],
+                "leftReps": entry["L Reps"],
+                "rightReps": entry["R Reps"],
+                "leftMaxForce": entry["L Max Force (N)"],
+                "rightMaxForce": entry["R Max Force (N)"],
+                "maxImbalance": entry["Max Imbalance"],
+                "leftMaxRatio": entry["L Max Ratio"],
+                "rightMaxRatio": entry["R Max Ratio"],
+                "leftAvgForce": entry["L Avg Force (N)"],
+                "rightAvgForce": entry["R Avg Force (N)"],
+                "avgImbalance": entry["Avg Imbalance"],
+                "leftAvgRatio": entry["L Avg Ratio"],
+                "rightAvgRatio": entry["R Avg Ratio"],
+                "leftMaxImpulse": entry["L Max Impulse (Ns)"],
+                "rightMaxImpulse": entry["R Max Impulse (Ns)"],
+                "impulseImbalance": entry["Impulse Imbalance (%)"]
+            }
+
+            # Step 3: Insert the record into your main table
+            response = supabase.table('forceframedata').insert(record).execute()
 
         return jsonify({"message": "Data inserted successfully!"}), 201
 
