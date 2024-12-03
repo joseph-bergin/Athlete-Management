701f8692856cdca35db2045acd62c90ef51b99e8
Matt Couts <74217539+mcouts01@users.noreply.github.com>
2024-12-03 14:56:15 -0600

    Update create_athlete_table.sql

--- db/create_athlete_table.sql
+++ db/create_athlete_table.sql
@@ -2,6 +2,7 @@ CREATE TABLE Athlete (
     athleteID BIGINT PRIMARY KEY,
     userID BIGINT,
     teamID BIGINT,
+    position_abbreviation varchar,
     FOREIGN KEY (userID) REFERENCES User(userID),
     FOREIGN KEY (teamID) REFERENCES Team(teamID)
 );
